a0080328da8851e754ee888145fede0550a1a47c
jhondoe256 <Jhondoe256@yahoo.com>
2020-06-01 10:58:22 -0400

    Add Client Class Identity model

---
+++ Social24.Data/Comment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Social24.Data
+{
+    public class Comment
+    {
+        [Key]
+        public int CommentId { get; set; }
+
+        [MaxLength(200, ErrorMessage ="There Are too many characters in this field.")]
+        public string Text { get; set; }
+        //public User Author { get; set; }
+        //public Post CommentPost {get; set;}
+    }
+}
